a20c804d3356e2fb1d192e3ef983f702da763b9b
anjeelaali <anjeelaali6@gmail.con>
2023-12-28 16:43:47 +0500

    added cards

---
+++ src/components/blogs/cards.js
@@ -0,0 +1,107 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import { Typography, Box } from '@mui/material';
+import BlogImgOne from '../../assets/blog_img_1.jpg';
+import BlogImgTwo from '../../assets/blog_img_2.jpg';
+import BlogImgThird from '../../assets/blog_img_3.jpg';
+import { GoDotFill } from "react-icons/go";
+
+
+
+
+const BlogCards = () => {
+
+    const data = [
+        {
+            id: 0,
+            image: BlogImgOne,
+            tagText: 'BUSSINESS',
+            text: '1 year ago',
+            heading: '5 reasons to purchase desktop computers',
+            content: 'Hustle and Cashflow is a blog that aims to educate millennials on personal finance.',
+
+        },
+        {
+            id: 1,
+            image: BlogImgTwo,
+            tagText: 'BUSSINESS',
+            text: '2 year ago',
+            heading: 'Utilizing mobile technology in the field',
+            content: 'Hustle and Cashflow is a blog that aims to educate millennials on personal finance.',
+
+        },
+        {
+            id: 2,
+            image: BlogImgThird,
+            tagText: 'BEAUTY',
+            text: '2 year ago',
+            heading: 'Success Story: Businessman in Harlem',
+            content: 'I think that you should be able to select more than one reason for rating.',
+
+        },
+    ]
+    return (
+        <>
+
+
+            {
+                data.map((item) => (
+                    <Card sx={{ maxWidth: 330, boxShadow:'1px 1px 33px -16px grey' }}>
+
+                        <Box sx={{
+                            height: '200px',
+                            overflow: 'hidden'
+                        }}>
+                            <img src={item.image} width='100%' />
+
+                        </Box>
+                        <CardContent>
+                            <Typography variant='h6' sx={{
+                                fontSize: '15px',
+                                fontWeight: '700'
+                            }}>
+                                Philip Reyes
+                                <Typography variant='para' sx={{
+                                    color: '#00000080',
+                                     marginLeft: '20px',
+                                    fontSize: '12px'
+                                }}>
+                                    <GoDotFill />
+
+                                </Typography>
+                                <Typography variant='para' sx={{
+                                    color: '#00000080',
+                                    margin: '0px 20px',
+                                    fontWeight: '400'
+                                }}>
+                                    {item.text}
+                                </Typography>
+                            </Typography>
+                            <Typography gutterBottom variant="h5" component="div" sx={{
+                                color:'#0e3f58',
+                                fontSize:'23px',
+                                fontWeight:'700',
+                                padding:'18px 0px'
+                            }}>
+                                {item.heading}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary" sx={{
+                                fontSize:'16px',
+                                color:'grey',
+                                paddingBottom:'15px'
+                            }}>
+                                {item.content}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                ))
+            }
+
+        </>
+    )
+}
+
+export default BlogCards
--- src/components/blogs/index.js
+++ src/components/blogs/index.js
@@ -3,52 +3,74 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
+import BlogCards from './cards';
 
 
 
 const Item = styled(Paper)(({ theme }) => ({
-    background:'none',
-    border:'1px solid blue',
-    padding:'20px 0px'
-  
-  }));
+    background: 'none',
+    padding: '20px 0px',
+    boxShadow: 'none'
+
+}));
 const LatestPosts = () => {
     return (
         <>
             <Box sx={{
-                border: '1px solid red',
                 padding: '20px',
-                width:'80%',
-                margin:'30px auto'
+                width: '80%',
+                margin: '30px auto'
             }}>
                 <Box sx={{ flexGrow: 1 }}>
                     <Grid container spacing={2}>
-                    <Grid item xs={6} md={4}>
+                        <Grid item xs={6} md={6}>
                             <Item>
                                 <Typography variant='para' sx={{
-                                    border:'1px solid red',
-                                    borderRadius:'40px',
-                                    padding:'5px',
-                                    
+                                    borderRadius: '40px',
+                                    padding: '5px',
+                                    color: '#000',
+                                    backgroundColor: '#1c4f8d1c',
+                                    fontSize: '10px',
+                                    fontWeight: '700',
+
+
                                 }}>
                                     SHARE YOUR STORY
                                 </Typography>
-                                <Typography variant='h3'>
-                                Latest Posts
+                                <Typography variant='h3' sx={{
+                                    color: '#45485f',
+                                    fontSize: '45px',
+                                    fontWeight: '700',
+                                    padding: '20px 0px'
+                                }}>
+                                    Latest Posts
                                 </Typography>
                             </Item>
                         </Grid>
-                        <Grid item xs={6} md={8}>
+                        <Grid item xs={6} md={6}>
                             <Item>
-                            <Typography variant='body2'>
-                            Our design services starts and ends with a best-in-class experience strategy that builds brands.
-                            </Typography>
+                                <Typography variant='body2' sx={{
+                                    fontSize: '20px',
+                                    color: '#505050',
+                                    marginTop: '20px'
+                                }}>
+                                    Our design services starts and ends with a best-in-class experience strategy that builds brands.
+                                </Typography>
                             </Item>
                         </Grid>
-                       
-                       
+
+
                     </Grid>
                 </Box>
+
+                <Box  sx={{
+                        margin: '30px 0px',
+                        padding: '20px 0px',
+                        display:'flex',
+                        justifyContent:'space-between'
+                    }}>                                                
+                                <BlogCards />
+                    </Box>
 
             </Box>
         </>
