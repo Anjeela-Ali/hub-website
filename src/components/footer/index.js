ff17b3060e07ce97caba5e78189f42a39e043600
anjeelaali <anjeelaali6@gmail.con>
2024-01-01 12:58:51 +0500

    Footer section added.

---
+++ src/components/footer/index.js
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const FooterSection = () => {
+  return (
+    <>
+    <Box>
+        Footer
+    </Box>
+      
+    </>
+  )
+}
+
+export default FooterSection
