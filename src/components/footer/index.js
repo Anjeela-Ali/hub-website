8d24e68707ce66d96b8f0155738c2bd3359dcaca
anjeelaali <anjeelaali6@gmail.con>
2024-01-03 16:56:08 +0500

    responsiveness for footer is commpleted.

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -40,64 +40,30 @@ function FooterSection() {
     return (
         <>
             <Box sx={{
-                backgroundColor: '#E5DFFC',       
-                
-                
+                backgroundColor: '#E5DFFC',
+
+
             }}>
                 <AppBar position="static" sx={{
                     backgroundColor: '#E5DFFC ',
-                    borderBottom:'1px solid #737588',
-                    width:'80%',
-                    margin:'10px auto',
-                    boxShadow:'none'
+                    borderBottom: '1px solid #737588',
+                    width: '80%',
+                    margin: '10px auto',
+                    boxShadow: 'none'
                 }}>
                     <Container maxWidth="xl">
-                        <Toolbar disableGutters>
-                            <img src={logoImgUrl} alt='screenshot'/>
-
-                            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                                <IconButton
-                                    size="large"
-                                    aria-label="account of current user"
-                                    aria-controls="menu-appbar"
-                                    aria-haspopup="true"
-                                    onClick={handleOpenNavMenu}
-                                    color="inherit"
-                                >
-                                    <MenuIcon />
-                                </IconButton>
-                                <Menu
-                                    id="menu-appbar"
-                                    anchorEl={anchorElNav}
-                                    anchorOrigin={{
-                                        vertical: 'bottom',
-                                        horizontal: 'left',
-                                    }}
-                                    keepMounted
-                                    transformOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'left',
-                                    }}
-                                    open={Boolean(anchorElNav)}
-                                    onClose={handleCloseNavMenu}
-                                    sx={{
-                                        display: { xs: 'block', md: 'none' },
-                                    }}
-                                >
-                                    {pages.map((page) => (
-                                        <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                            <Typography textAlign="center">{page}</Typography>
-                                        </MenuItem>
-                                    ))}
-                                </Menu>
-                            </Box>
-                            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'right' }}>
+                        <Toolbar disableGutters sx={{
+                            display: { xs: 'block', sm: 'block', md: 'block', lg: 'flex', xl: 'block' },
+                            padding: '20px 0px'
+                        }}>
+                            <img src={logoImgUrl} alt='screenshot' />
+                            <Box sx={{ display: { xs: 'flex', md: 'flex' }, marginLeft: { xs: '0px', sm: '0px', md: '0px', lg: '140px', xl: '140px' }, flexWrap: 'wrap', marginTop:{ xs: '20px', sm: '20px', md: '20px', lg: '0px', xl: '0px' }}}>
                                 {pages.map((page) => (
                                     <Button
                                         key={page}
                                         onClick={handleCloseNavMenu}
                                         sx={{
-                                            my: 2, display: 'block', color: '#000000cc', padding: '20px', fontWeight: '600', fontSize: '12px',
+                                            display: 'block', color: '#000000cc', fontWeight: '600', fontSize: '12px', paddingRight: '20px',
                                             '&:hover': {
                                                 color: '#2d6fe8'
                                             }
@@ -108,65 +74,40 @@ function FooterSection() {
                                 ))}
                             </Box>
 
-                            <Box sx={{ flexGrow: 0 }}>
-
-                                <Menu
-                                    sx={{ mt: '45px' }}
-                                    id="menu-appbar"
-                                    anchorEl={anchorElUser}
-                                    anchorOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'right',
-                                    }}
-                                    keepMounted
-                                    transformOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'right',
-                                    }}
-                                    open={Boolean(anchorElUser)}
-                                    onClose={handleCloseUserMenu}
-                                >
-                                    {settings.map((setting) => (
-                                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                            <Typography textAlign="center">{setting}</Typography>
-                                        </MenuItem>
-                                    ))}
-                                </Menu>
-                            </Box>
                         </Toolbar>
                     </Container>
                 </AppBar>
 
                 {/* Content */}
                 <Box sx={{
-                    width:'80%',
-                    margin:'0px auto',
-                    display:'flex',
-                    justifyContent:'space-between',
-                    padding:'40px 0px'
+                    width: '80%',
+                    margin: '0px auto',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    padding: '40px 0px'
                 }}>
                     <Box >
                         <Typography variant='h6' sx={{
-                            fontSize:'13px',
-                            fontWeight:'600',
-                            paddingRight:'40px',
-                            color:'#737588'
+                            fontSize: '13px',
+                            fontWeight: '600',
+                            paddingRight: '40px',
+                            color: '#737588'
                         }}>
-                        These Terms will be applied fully and affect to your use of this Website. By using this Website, you agreed to accept all terms <br /> and conditions written in here. You must not use this Website if you disagree with any of these Website Standard Terms and  <br />Conditions.
+                            These Terms will be applied fully and affect to your use of this Website. By using this Website, you agreed to accept all terms <br /> and conditions written in here. You must not use this Website if you disagree with any of these Website Standard Terms and  <br />Conditions.
                         </Typography>
                     </Box>
                     <Box sx={{
-                        fontSize:'30px',
-                        padddingRight:'20px',
-                        color:'#225469',
-                        width:'10%',
+                        fontSize: '30px',
+                        padddingRight: '20px',
+                        color: '#225469',
+                        width: '10%',
 
                     }}>
-                    <IoLogoInstagram />
-                    <Typography variant='para' sx={{
-                        padding:'0px 10px'
-                    }}></Typography>
-                    <FaTwitter />
+                        <IoLogoInstagram />
+                        <Typography variant='para' sx={{
+                            padding: '0px 10px'
+                        }}></Typography>
+                        <FaTwitter />
 
                     </Box>
                 </Box>
